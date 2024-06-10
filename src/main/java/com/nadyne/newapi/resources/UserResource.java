ec96c3d0ba614990d81e6b4a1595cb1404f61ebb
Nadyne Barbieri <97990821+NadyneBarbieri@users.noreply.github.com>
2024-06-10 17:23:13 -0300

    Nova API

---
+++ src/main/java/com/nadyne/newapi/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.nadyne.newapi.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nadyne.newapi.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Nadyne", "nadyne@gmail.com", "12345678");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+}
